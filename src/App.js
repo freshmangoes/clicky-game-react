3a8082672ac28cc83b8edb8dba21208737c6d628
Kyle C <kylecilia@gmail.com>
2020-03-07 16:49:15 -0800

    Adds game component to house images

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navbar from "./components/Navbar"
 import Jumbotron from "./components/Jumbotron"
 import Wrapper from './components/Wrapper';
+import Game from './components/Game'
 
 function App() {
   return (
     <Wrapper>
       <Navbar />
       <Jumbotron />
+      <Game />
     </Wrapper>
   );
 }
