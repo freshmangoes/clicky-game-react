62a4a6a188a3fc968fa7c2ffb3b7d24bb3983495
Kyle C <kylecilia@gmail.com>
2020-03-09 19:14:12 -0700

    Adds images to a jsx component so they can be imported and used

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import imgs from '../../imgs.json';
+import React, { useState } from 'react';
+import Images from '../Images/';
 import Picture from '../Picture/';
 
 const Game = () => {
+  // state for imgs array
+  const [imgs, setImgs] = useState(Images);
+
+  // state for array of clicked imgs
+  const [clicked, setClicked] = useState({});
+
+  // state for score
+  const [score, setScore] = useState(0);
+
+  console.log('imgs', imgs);
+  console.log('clicked', clicked);
+  console.log('score', score);
+
 	return (
 		<div className="container-fluid">
-			{imgs.map((img) => {
-				return <Picture key={img.id} href={img.href} name={img.name} />;
-			})}
+			<div className="col">
+				<div className="row d-flex">
+					{imgs.map((img) => {
+						return <Picture key={img.id} href={img.href} name={img.name} />;
+					})}
+				</div>
+			</div>
 		</div>
 	);
 };
---
+++ src/components/images/Images.js
@@ -0,0 +1,34 @@
+const Images = [
+	{
+		id: 1,
+		name: 'cayde',
+		href: require('./cayde.png')
+	},
+	{
+		id: 2,
+		name: 'zavala',
+		href: require('./zavala.png')
+	},
+	{
+		id: 3,
+		name: 'ikora',
+		href: require('./ikora.png')
+	},
+	{
+		id: 4,
+		name: 'orisis',
+		href: require('./osiris.png')
+	},
+	{
+		id: 5,
+		name: 'saladin',
+		href: require('./saladin.png')
+	},
+	{
+		id: 6,
+		name: 'shaxx',
+		href: require('./shaxx.png')
+	}
+];
+
+export default Images;
