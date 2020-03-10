87f700fe41a21e5dc37f179ea6b60b57e5a168fb
Kyle C <kylecilia@gmail.com>
2020-03-09 20:29:28 -0700

    Shuffles image array, need to have array map called on a different component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,21 +3,53 @@ import Images from '../Images/';
 import Picture from '../Picture/';
 
 const Game = () => {
-  // state for imgs array
-  const [imgs, setImgs] = useState(Images);
+	// state for imgs array
+	const [imgs, setImgs] = useState(Images);
 
-  // state for score
-  const [score, setScore] = useState(0);
+	// state for score
+	const [score, setScore] = useState(0);
 
-  console.log('imgs', imgs);
-  console.log('score', score);
+	console.log('imgs', imgs);
+	console.log('score', score);
+
+	// TODO test this thing
+	// presumably shuffles images
+	const shuffleArray = (array) => {
+		const imgArray = array;
+		console.log('clicked');
+		for (let i = 0; i < imgArray.length; i++) {
+			let index = Math.floor(Math.random() * i);
+			let temp = imgArray[i];
+			imgArray[i] = imgArray[index];
+			imgArray[index] = temp;
+		}
+		setImgs(imgArray);
+		console.log('imgs', imgs);
+  };
+  
+  // click handler
+  const handleImgClick = () => {
+    shuffleArray(imgs);
+  }
+
+  // NOTE debug
+  console.log('rendering');
 
 	return (
 		<div className="container-fluid">
 			<div className="col">
 				<div className="row d-flex">
+          {/* NOTE */}
+          {/* Might have to place this bit in another component? */}
 					{imgs.map((img) => {
-						return <Picture key={img.id} src={img.src} name={img.name}/>;
+						return (
+							<Picture
+								key={img.id}
+								src={img.src}
+								name={img.name}
+								handleImgClick={handleImgClick}
+							/>
+						);
 					})}
 				</div>
 			</div>
--- src/components/Picture/Picture.js
+++ src/components/Picture/Picture.js
@@ -1,25 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 const Picture = (props) => {
-  // clicked state for each picture element
-  // initially false
-  const [clicked, setClicked] = useState({});
-  
-  // NOTE debug
-  console.log('props.name', props.name);
-  console.log('clicked', clicked);
-  
-  // Click handler function
-  const handleClick = () => {
-    setClicked(true);
-  }
+  let { src, id, name } = props;
+    
+	return (
+		<div>
+			<img alt={name} src={src} charid={id} onClick={props.handleImgClick} />
+		</div>
+	);
+};
 
-  let {src, id, name} = props;
-  return (
-    <div>
-      <img alt={name} src={src} charid={id} onClick={handleClick}/>
-    </div>
-  )
-}
-
-export default Picture
+export default Picture;
