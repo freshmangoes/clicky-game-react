8a0aa320654323933287bbd54a51e9a90ddefc6c
Kyle C <kylecilia@gmail.com>
2020-03-11 09:11:17 -0700

    Still at the same place where the images array gets shuffled but not rerendered [WIP]

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,55 +1,15 @@
 import React, { useState } from 'react';
-import Images from '../Images/';
-import Picture from '../Picture/';
+import Pictures from '../Pictures';
 
 const Game = () => {
-	// state for imgs array
-	const [imgs, setImgs] = useState(Images);
-
-	// state for score
-	const [score, setScore] = useState(0);
-
-	console.log('imgs', imgs);
-	console.log('score', score);
-
-	// TODO test this thing
-	// presumably shuffles images
-	const shuffleArray = (array) => {
-		const imgArray = array;
-		console.log('clicked');
-		for (let i = 0; i < imgArray.length; i++) {
-			let index = Math.floor(Math.random() * i);
-			let temp = imgArray[i];
-			imgArray[i] = imgArray[index];
-			imgArray[index] = temp;
-		}
-		setImgs(imgArray);
-		console.log('imgs', imgs);
-  };
-  
-  // click handler
-  const handleImgClick = () => {
-    shuffleArray(imgs);
-  }
-
-  // NOTE debug
-  console.log('rendering');
+	// NOTE debug
+	console.log('rendering');
 
 	return (
 		<div className="container-fluid">
 			<div className="col">
 				<div className="row d-flex">
-          {/* NOTE Might have to place this bit in another component? */}
-					{imgs.map((img) => {
-						return (
-							<Picture
-								key={img.id}
-								src={img.src}
-								name={img.name}
-								handleImgClick={handleImgClick}
-							/>
-						);
-					})}
+          <Pictures />
 				</div>
 			</div>
 		</div>
--- src/components/Picture/Picture.js
+++ src/components/Picture/Picture.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Picture = (props) => {
-  let { src, id, name } = props;
-    
+	let { src, id, name, handleImgClick } = props;
+
 	return (
 		<div>
-			<img alt={name} src={src} charid={id} onClick={props.handleImgClick} />
+			<img alt={name} src={src} charid={id} onClick={handleImgClick} />
 		</div>
 	);
 };
